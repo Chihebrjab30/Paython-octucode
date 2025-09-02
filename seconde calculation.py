a5745818da223b697a4a128fad8b76e6bc9de2b7
Chihebrjab30 <chihebrjab30@gmail.com>
2025-09-02 17:56:17 +0100

    Add files via upload

---
+++ seconde calculation.py
@@ -0,0 +1,13 @@
+seconde=int(input("entre all seconde :\n"))
+#deeys=seconde//86400
+#hours=seconde%86400 //3600
+#seconde=seconde%3600
+#print(deeys)
+#print(hours)
+#print(seconde)
+print(f"{seconde//86400} D {seconde%86400//3600} H {seconde%3600}S")
+
+
+
+
+
