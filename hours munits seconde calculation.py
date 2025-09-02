7a53a0b94f6e4f77d4ebb128736f673edc54d7e7
Chihebrjab30 <chihebrjab30@gmail.com>
2025-09-02 17:57:12 +0100

    Add files via upload

---
+++ hours munits seconde calculation.py
@@ -0,0 +1,15 @@
+print("welcome to time calculator:")
+time=input("entre seconde here :\n")
+total=int(time)
+hours=total//3600
+munite=total%3600//60
+seconde=total%60
+print("video hours is:"+str(hours)+" H ")
+print("video munite is:"+str(munite)+" M ")
+print("video secone is :"+str(seconde)+" S ")
+
+
+
+
+
+
