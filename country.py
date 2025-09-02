afcbafab6b0d3636b85519698ed3c284b283c67f
Chihebrjab30 <chihebrjab30@gmail.com>
2025-09-02 18:05:29 +0100

    Add files via upload

---
+++ country.py
@@ -0,0 +1,9 @@
+country=input("entre country here\n")
+if country.lower()=="tunisia" or country.lower()=="libya" or country.lower()=="algeria" :
+    print(f"{country} is accepted")
+else:
+    print(f"sorry your {country} is not aceepted")
+    
+    
+    
+    
