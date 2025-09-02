f80745a2e477cdd2c710dc814f74ed14ba3da977
Chiheb Rj-7 <chihebrjab30@gmail.com>
2025-09-02 23:41:19 +0100

    Update country_info.py

--- country_info.py
+++ country_info.py
@@ -2,8 +2,4 @@ country=input("entre country here\n")
 if country.lower()=="tunisia" or country.lower()=="libya" or country.lower()=="algeria" :
     print(f"{country} is accepted")
 else:
-    print(f"sorry your {country} is not aceepted")
-    
-    
-    
-    
+    print(f"sorry your {country} is not aceepted")    
